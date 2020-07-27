b4ee4e9be036a94256a7dcdf1e41b500ad897093
DvRocker <42909244+DvRocker@users.noreply.github.com>
2020-07-27 21:58:51 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NzM2OTg0OTU0ODk3OTU2OTc1.Xx2wuQ.pZa-VJbcHDyDN6bzDEk1M7w9KyU',    //هون توكن البوت
+    YT_API_KEY: 'api ', 
+    prefix: '$',
+    devs: ['358529506052669440']//هون ايديك 
+}
